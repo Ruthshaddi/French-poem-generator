cc0d35d1c3afebfed8bdae56e7802034257f2a1d
Ruthshaddi <odusanyaruth26@gmail.com>
2025-01-18 09:40:53 +0100

    Added basic form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-alert("helo world");
+function generatepoem(event) {
+    event.preventDefault();
+
+let poemElement = document.querySelector("#poem");
+poemElement.innerHTML = "La tombe dit à la rose" ;
+
+
+new Typewriter('poem', {
+  strings: ["La tombe dit à la " ],
+  autoStart: true,
+  delay : 1,
+  cursor: "",
+});
+}
+
+
+
+
+let poemFormElement= document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit",generatepoem)
